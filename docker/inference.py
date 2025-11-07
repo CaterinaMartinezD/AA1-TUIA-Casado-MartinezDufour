3900314b307c00e88f2d88b417351185ac224ac5
gcasado0 <casado.gustavo@gmail.com>
2025-11-07 20:15:56 -0300

    Agregado seccion 10 MLops

---
+++ docker/inference.py
@@ -0,0 +1,35 @@
+import joblib
+import pandas as pd
+import warnings
+import sklearn
+print(sklearn.__version__)
+warnings.simplefilter('ignore')
+
+import logging
+from sys import stdout
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logFormatter = logging.Formatter("%(asctime)s %(levelname)s %(filename)s: %(message)s")
+consoleHandler = logging.StreamHandler(stdout)
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+pipeline = joblib.load('pipeline.pkl')
+
+logger.info('loaded pipeline')
+
+df_input = pd.read_csv('/files/input.csv')
+
+logger.info('loaded input')
+
+print("shape:", df_input.shape)
+print(df_input.head())
+
+output = pipeline.predict(df_input)
+
+logger.info('made predictions')
+
+pd.DataFrame(output, columns=['Rain_predicted']).to_csv('/files/output.csv', index=False)
+
+logger.info('saved output')
